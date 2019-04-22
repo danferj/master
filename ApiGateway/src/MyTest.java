d71a06f613c49f7cb4b26412334b4f9c4fbae341
acc_ma_z <zhanbao.ma@DLC0009W1LDK2D7.dir.svc.accenture.com>
2019-04-22 17:17:04 +0800

    test2

--- ApiGateway/src/MyTest.java
+++ ApiGateway/src/MyTest.java
@@ -4,6 +4,10 @@ public class MyTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("-----OK-----");
+		System.out.println("-----OK2-----");
+		System.out.println("-----OK3-----");
+		System.out.println("-----OK4-----");
+		System.out.println("-----OK5-----");
 	}
 
 }
